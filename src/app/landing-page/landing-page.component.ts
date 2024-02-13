1a44c4cf41d1c2caeeae836a91ba7ac81fd6cbef
TechMadi <winnieamandela@gmail.com>
2024-02-14 00:13:52 +0300

    Fix:  Adding a loader

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css']
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit {
+
+  isloading$=new BehaviorSubject<boolean>(false);
+
+
+ ngOnInit(): void {
+    this.isloading$.next(true);
+    setTimeout(() => {
+      this.isloading$.next(false);
+    }, 2000);
+  }
 
 }
+
+
