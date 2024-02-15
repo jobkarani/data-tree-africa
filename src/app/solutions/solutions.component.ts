27210fef511cec2f84dfabbdd2b4b60e43f7b9f4
Job Karani <jobkarani084j@gmail.com>
2024-02-16 00:57:21 +0300

    fix: african touch

--- src/app/solutions/solutions.component.ts
+++ src/app/solutions/solutions.component.ts
@@ -7,11 +7,11 @@ import { Component } from '@angular/core';
 })
 
 export class SolutionsComponent {
-  selectedTab = '01 Data Insights';
-  insightsData = ['Unearth hidden gems: Leverage our expert-crafted custom analyses or explore industry/sector reports to gain actionable insights from your data. See it unfold: Interactively explore powerful data sets and tools, visualizing insights in real-time for quicker decision-making.', './assets/images/Analysis-cuate 1.svg'];
-  publishingData = ['Fuel smarter decisions: Access an ocean of high-quality data across diverse sectors, readily available through APIs or convenient CSV downloads.', './assets/images/data-publishing.svg'];
-  toolingData = ['Unleash the full potential: Integrate seamlessly with your existing data environment, unlocking the power of your combined data ecosystem. Data-driven decisions: Empower your teams with real-time analytics, transforming data into actionable insights on the fly.', './assets/images/data-tooling.svg'];
-  cloudData = ['Cloud innovation, simplified: Leverage our advanced cloud services to optimize storage, processing, and analysis, unlocking seamless scalability and agility.', './assets/images/cloud_computing.svg'];
+  selectedTab = '01 Powerful Data & Analytics';
+  insightsData = ['Access high-quality African data across diverse sectors, readily available through APIs or convenient CSV downloads. Interact with our powerful data sets and quickly visualize insights to power your decision-making. Custom analyses and sector reports.', './assets/images/Analysis-cuate 1.svg'];
+  publishingData = ['Fuel smarter decisions: Access an ocean of high-quality data across diverse sectors, readily available through APIs or convenient CSV downloads.', './assets/images/data-publishingb.svg'];
+  toolingData = ['Integrate our technology and tools into your data environment, and unleash the potential of your data. Empower your teams with real-time analytics, transforming your data into actionable insights on the fly.', './assets/images/data_tooling 1.svg'];
+  cloudData = ['Cloud innovation, simplified: Leverage our advanced cloud services to optimize storage, processing, and analysis, unlocking seamless scalability and agility.', './assets/images/cloud-computing1 1.svg'];
 
   onTabClick(tabName: string) {
     this.selectedTab = tabName;
@@ -19,11 +19,11 @@ export class SolutionsComponent {
 
   getSelectedData(index: number): string {
     switch (this.selectedTab) {
-      case '01 Data Insights':
+      case '01 Powerful Data & Analytics':
         return this.insightsData[index];
       case '02 Data Publishing':
         return this.publishingData[index];
-      case '03 Data Tooling':
+      case '03 Data Tools':
         return this.toolingData[index];
       case '04 Cloud Solutions':
         return this.cloudData[index];
