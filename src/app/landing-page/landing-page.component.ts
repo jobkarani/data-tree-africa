6bbd0137949df8a6f5f698ed5beeffa8477ff700
Job Karani <jobkarani084j@gmail.com>
2024-02-16 02:01:02 +0300

    feat: SEO meta keywords

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-landing-page',
@@ -10,8 +11,14 @@ export class LandingPageComponent implements OnInit {
 
   isloading$=new BehaviorSubject<boolean>(false);
 
+  constructor(private meta: Meta) {};
 
  ngOnInit(): void {
+    this.meta.addTags([ 
+      { name: 'description', content: 'Data Tree Africa' }, 
+      { name: 'keywords', content: 'Data Center Nairobi Kenya, Data Tree Africa, African Data Analysis, Big Data Africa, Cloud Computing Africa, Data Analytics East Africa, AI Africa, Machine Learning Africa, African Market Research, Data Security Africa, African Data Privacy, Colocation Nairobi, Disaster Recovery Nairobi, Cloud Services Africa, High Performance Computing Africa, Internet Exchange Point Kenya, Data Governance Africa, African Data Insights, Business Intelligence Africa, Sustainable Data Center Africa, Fintech Data Africa' } 
+    ]);
+
     this.isloading$.next(true);
     setTimeout(() => {
       this.isloading$.next(false);
